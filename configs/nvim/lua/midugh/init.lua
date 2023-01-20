ba2f8067d9e95a30a5e0203bfc2cc87cbcc1e53e
m1dugh <romain103paris@gmail.com>
2023-01-20 17:54:19 +0100

    adds parameters to neovim config

--- configs/nvim/lua/midugh/init.lua
+++ configs/nvim/lua/midugh/init.lua
@@ -6,5 +6,8 @@ set.shiftwidth = 4
 set.expandtab = true
 set.number = true
 set.relativenumber = true
+set.cc = "80"
 
 vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "+", function () vim.cmd("m -2") end)
+vim.keymap.set("n", "-", function () vim.cmd("m +1") end)
