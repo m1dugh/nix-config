3e5970f6a0f5ab9bdefa82b99a103d94b31bd674
m1dugh <romain103paris@gmail.com>
2023-11-29 17:46:24 +0100

    feat(modules): changes nvim to proper module

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,19 +1,34 @@
 {
     pkgs,
+    config,
+    lib,
     ...
 }:
-{
-    home.file.".config/nvim" = {
-        recursive = true;
-        source = ./config;
+with lib;
+let cfg = config.midugh.nvim;
+in {
+    options.midugh.nvim = {
+        enable = mkEnableOption "nvim default config";
+        useAliases = mkOption {
+            description = "use `vi` and `vim` as aliases for `nvim`";
+            type = types.bool;
+            default = true;
+        };
     };
 
-    programs.neovim = {
-        enable = true;
-        viAlias = true;
-        vimAlias = true;
-        plugins = [
-            pkgs.vimPlugins.packer-nvim
-        ];
+    config = mkIf cfg.enable {
+        home.file.".config/nvim" = {
+            recursive = true;
+            source = ./config;
+        };
+
+        programs.neovim = {
+            enable = true;
+            viAlias = cfg.useAliases;
+            vimAlias = cfg.useAliases;
+            plugins = [
+                pkgs.vimPlugins.packer-nvim
+            ];
+        };
     };
 }
