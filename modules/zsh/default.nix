448147f969d0369f119e21bb01f542109a0ccc22
m1dugh <romain103paris@gmail.com>
2023-11-29 17:46:32 +0100

    feat(modules): changes zsh to proper module

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,43 +1,67 @@
 {
-    withKubernetes ? false,
-    historySize ? 100000,
-}:
-{
+    lib,
+    config,
     ...
 }:
-{
-    programs.zsh = {
-        enable = true;
-        enableAutosuggestions = true;
-        syntaxHighlighting.enable = true;
-        oh-my-zsh = {
+with lib;
+let cfg = config.midugh.zsh;
+in {
+    options.midugh.zsh = {
+        enable = mkEnableOption "zsh default config";
+
+        historySize = mkOption {
+            type = types.int;
+            default = 100000;
+            description = "The size of the history";
+        };
+
+        theme = mkOption {
+            type = types.str;
+            default = "robbyrussell";
+            description = "The OhMyZsh theme";
+        };
+
+        withKubernetes = mkOption {
+            type = types.bool;
+            default = false;
+            description = "Whether to add kubernetes auto complete";
+        };
+    };
+
+    config = mkIf cfg.enable {
+        programs.zsh = {
             enable = true;
-            plugins = [
-                "git"
-            ];
-            theme = "robbyrussell";
-        };
+            enableAutosuggestions = true;
+            syntaxHighlighting.enable = true;
+            oh-my-zsh = {
+                enable = true;
+                plugins = [
+                    "git"
+                ];
+                theme = cfg.theme;
+            };
 
-        history = {
-            size = historySize;
-            save = historySize;
-        };
+            history = {
+                size = cfg.historySize;
+                save = cfg.historySize;
+            };
 
-        initExtra = (if withKubernetes then ''
-            if (( $+commands[kubectl] )); then
-                # If the completion file doesn't exist yet, we need to autoload it and
-                # bind it to `kubectl`. Otherwise, compinit will have already done that.
-                if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
-                  typeset -g -A _comps
-                  autoload -Uz _kubectl
-                  _comps[kubectl]=_kubectl
+            initExtra = (strings.optionalString cfg.withKubernetes ''
+                if (( $+commands[kubectl] )); then
+                    # If the completion file doesn't exist yet, we need to autoload it and
+                    # bind it to `kubectl`. Otherwise, compinit will have already done that.
+                    if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
+                      typeset -g -A _comps
+                      autoload -Uz _kubectl
+                      _comps[kubectl]=_kubectl
+                    fi
+
+                    kubectl completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
                 fi
-
-                kubectl completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
-            fi
-            '' else "");
-        shellAliases = (if withKubernetes then {
-            k = "kubectl";
-        } else {});
+                '');
+            shellAliases = (mkIf cfg.withKubernetes {
+                k = "kubectl";
+            });
+        };
     };
 }
