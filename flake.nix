39f9152041e1c0edc15adfeba05bbfef9fad951a
m1dugh <romain103paris@gmail.com>
2024-03-26 15:34:48 +0100

    feat(packages): adds gdb

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
         home-manager = {
             url = "github:nix-community/home-manager/release-23.11";
@@ -13,6 +14,7 @@
     outputs = {
         self,
         nixpkgs,
+        nixpkgs-unstable,
         home-manager,
         ...
     }: 
@@ -20,10 +22,20 @@
     let
         system = "x86_64-linux";
         username = "midugh";
+        overlay = final: prev: 
+        let pkgs-unstable = import nixpkgs-unstable {
+            inherit system;
+        };
+        in {
+            gdb = pkgs-unstable.gdb;
+        };
         pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true;
             config.allowUnsupportedSystem = true;
+            overlays = [
+                overlay
+            ];
         };
     in {
         packages.${system}.home-manager = home-manager.defaultPackage.${system};
