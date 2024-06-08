4b9550ed3fbb72e58f25fb009d1cd935cfa0ce43
m1dugh <romain103paris@gmail.com>
2024-06-08 19:51:05 +0200

    feat(zsh): enhances zsh prompt

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -53,20 +53,24 @@ in {
         fi
       '')
 
+      ''
+      zmodload zsh/complist
+      bindkey '^E' autosuggest-accept
+      bindkey '^[[1;5C' forward-word
+      bindkey '^[[1;5D' backward-word
+      fpath=(${./prompt} $fpath)
+      autoload -Uz prompt_custom_setup && prompt_custom_setup
+      ''
+
       (strings.optionalString cfg.viMode ''
        bindkey -v
        export KEYTIMEOUT=1
-       zmodload zsh/complist
        bindkey -M menuselect 'h' vi-backward-char
        bindkey -M menuselect 'k' vi-up-line-or-history
        bindkey -M menuselect 'l' vi-forward-char
        bindkey -M menuselect 'j' vi-down-line-or-history
       '')
 
-      ''
-      fpath=(${./prompt} $fpath)
-      autoload -Uz prompt_custom_setup && prompt_custom_setup
-      ''
       ];
       shellAliases = lib.mkMerge 
       [
