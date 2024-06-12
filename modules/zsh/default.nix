39448fb50e5d5efeeb60857f42f303ddb25aa5b2
m1dugh <romain103paris@gmail.com>
2024-06-12 18:33:08 +0200

    fix(zsh): fixes kubectl completion

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -39,18 +39,11 @@ in {
       };
 
       initExtra = strings.concatStringsSep "\n" [
+      ''
+      ZSH_CACHE_DIR=''${ZSH_CACHE_DIR:-~/.config/zsh/}
+      ''
       (strings.optionalString cfg.withKubernetes ''
-        if (( $+commands[kubectl] )); then
-            # If the completion file doesn't exist yet, we need to autoload it and
-            # bind it to `kubectl`. Otherwise, compinit will have already done that.
-            if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
-              typeset -g -A _comps
-              autoload -Uz _kubectl
-              _comps[kubectl]=_kubectl
-            fi
-
-            kubectl completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
-        fi
+        source <(kubectl completion zsh)
       '')
 
       ''
