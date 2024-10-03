a663cc8b4f1b05068732cf9ab35453927e482948
m1dugh <romain.le-miere@epita.fr>
2024-10-03 23:46:40 +0200

    feat(zsh): adds begining search for up and down commands

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -69,10 +69,18 @@ in {
           bindkey -v
           export KEYTIMEOUT=1
           bindkey -M menuselect 'h' vi-backward-char
-          bindkey -M menuselect 'k' vi-up-line-or-history
+          bindkey -M vicmd 'k' up-line-or-beginning-search
           bindkey -M menuselect 'l' vi-forward-char
-          bindkey -M menuselect 'j' vi-down-line-or-history
-        '')
+          bindkey -M vicmd 'j' down-line-or-beginning-search
+          '')
+        ''
+        autoload -U up-line-or-beginning-search
+        autoload -U down-line-or-beginning-search
+        zle -N up-line-or-beginning-search
+        zle -N down-line-or-beginning-search
+        bindkey "$terminfo[kcuu1]" up-line-or-beginning-search # Up
+        bindkey "$terminfo[kcud1]" down-line-or-beginning-search # Down
+        ''
       ]);
     in
     mkIf cfg.enable {
