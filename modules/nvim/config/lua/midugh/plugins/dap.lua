fc5f7af64c9b9be30f591e03c4c73dab3f956641
m1dugh <romain.le-miere@epita.fr>
2025-10-16 02:10:18 +0200

    feat(nvim): adds gitsigns

--- modules/nvim/config/lua/midugh/plugins/dap.lua
+++ modules/nvim/config/lua/midugh/plugins/dap.lua
@@ -1,10 +1,4 @@
 return {
 	"mfussenegger/nvim-dap",
 	tag = "0.10.0",
-	dependencies = {
-		{
-			"mfussenegger/nvim-dap-python",
-			opts = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python",
-		},
-	},
 }
