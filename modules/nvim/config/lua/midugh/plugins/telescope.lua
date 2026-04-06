8e29b830c9877cc2c7b704da5aa3427440b0d4c3
m1dugh <romain.le-miere@epita.fr>
2026-04-06 17:16:35 +0200

    fix(nvim): adds preview for telescope

--- modules/nvim/config/lua/midugh/plugins/telescope.lua
+++ modules/nvim/config/lua/midugh/plugins/telescope.lua
@@ -1,15 +1,30 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
+	tag = "v0.2.1",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("telescope").setup()
+		-- local previewers = require("telescope.previewers")
+
+		require("telescope").setup({
+			defaults = {
+				layout_config = {
+					horizontal = {
+						preview_cutoff = 0,
+					},
+				},
+			},
+		})
 
 		local builtin = require("telescope.builtin")
+
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+
 		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			builtin.grep_string({
+				search = vim.fn.input("Grep > "),
+			})
 		end, {})
 	end,
 }
